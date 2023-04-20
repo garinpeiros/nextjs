372cbec1b8f62c42c04416bfd305f032a6c80a71
mitsuyoshi sakamoto <kokoperi3244@icloud.com>
2023-04-20 14:21:34 +0900

    no message

--- next.config.js
+++ next.config.js
@@ -1,24 +1,16 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
+module.exports = {
   async headers() {
     return [
       {
         // matching all API routes
-
         source: "/api/:path*",
-
         headers: [
           { key: "Access-Control-Allow-Credentials", value: "true" },
-
           { key: "Access-Control-Allow-Origin", value: "*" },
-
           {
             key: "Access-Control-Allow-Methods",
             value: "GET,OPTIONS,PATCH,DELETE,POST,PUT",
           },
-
           {
             key: "Access-Control-Allow-Headers",
             value:
@@ -29,5 +21,3 @@ const nextConfig = {
     ]
   },
 }
-
-module.exports = nextConfig
